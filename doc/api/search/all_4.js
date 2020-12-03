0fc09854dcb851401bf13cec83707c4ed90174d2
banalna <judas.priest999@gmail.com>
2020-12-03 13:14:09 +0300

    delete component.conf.ps1 and doc folder

--- doc/api/search/all_4.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['build',['Build',['../namespace_pip_services_1_1_redis_1_1_build.html',1,'PipServices3::Redis']]],
-  ['cache',['Cache',['../namespace_pip_services_1_1_redis_1_1_cache.html',1,'PipServices3::Redis']]],
-  ['lock',['Lock',['../namespace_pip_services_1_1_redis_1_1_lock.html',1,'PipServices3::Redis']]],
-  ['pipservices',['PipServices3',['../namespace_pip_services.html',1,'']]],
-  ['redis',['Redis',['../namespace_pip_services_1_1_redis.html',1,'PipServices3']]]
-];
--- doc/api/search/functions_5.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['setreferences',['SetReferences',['../class_pip_services_1_1_redis_1_1_cache_1_1_redis_cache.html#a26b2415d7a196aabf8124835d762e554',1,'PipServices3.Redis.Cache.RedisCache.SetReferences()'],['../class_pip_services_1_1_redis_1_1_lock_1_1_redis_lock.html#a4717ba26d94dcedc35900baafdb930cf',1,'PipServices3.Redis.Lock.RedisLock.SetReferences()']]],
-  ['storeasync_3c_20t_20_3e',['StoreAsync&lt; T &gt;',['../class_pip_services_1_1_redis_1_1_cache_1_1_redis_cache.html#afe616f992149d5da811ed83a61751a69',1,'PipServices3::Redis::Cache::RedisCache']]]
-];
